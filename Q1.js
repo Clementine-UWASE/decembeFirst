b13129550ca655f2468ce2176a03449a0f63e56f
UWASE Clementine <clemawase@gmail.com>
2023-12-01 05:45:57 +0200

    first commit

---
+++ Q1.js
@@ -0,0 +1,8 @@
+let train ={
+    from:"Nyabugogo",
+    to:"100km",
+    price:2000,
+};
+console.log(train.from);
+console.log(train.to);
+console.log(train.price);
---
+++ Q2.js
@@ -0,0 +1,5 @@
+let person ={};
+person.name = "Clementine",
+person.sername = "UWASE";
+console.log(person.name);
+console.log(person.sername);
